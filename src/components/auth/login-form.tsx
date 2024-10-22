e7f1ed1efb089f441e75528f0a633fa809875419
alfredosdpiii <alfredo.sdpiii@gmail.com>
2024-10-22 21:03:01 +0800

    login component

---
+++ src/components/auth/login-form.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { AuthCard } from "@/components/auth/auth-card";
+
+export const LoginForm = () => {
+  return (
+    <AuthCard
+      cardTitle="Welcome back!"
+      backButtonHref="/auth/register"
+      backButtonLabel="Create a new account"
+      showSocials
+    >
+    </AuthCard>
+  );
+};
