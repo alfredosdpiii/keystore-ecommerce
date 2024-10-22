7d64d466ff07904290eae34986c936478eee6564
alfredosdpiii <alfredo.sdpiii@gmail.com>
2024-10-23 00:08:25 +0800

    add login form

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,8 +1,36 @@
 "use client";
 
 import { AuthCard } from "@/components/auth/auth-card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginSchema } from "@/types/login-schema";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import * as z from "zod";
 
 export const LoginForm = () => {
+  const form = useForm({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    console.log(values);
+  };
+
   return (
     <AuthCard
       cardTitle="Welcome back!"
@@ -10,7 +38,52 @@ export const LoginForm = () => {
       backButtonLabel="Create a new account"
       showSocials
     >
-      <div></div>
+      <div>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-4"
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="someone@email.com"
+                      {...field}
+                      type="email"
+                      autoComplete="email"
+                    />
+                  </FormControl>
+                  <FormDescription>Input your email</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="******" {...field} type="password" />
+                  </FormControl>
+                  <FormDescription>Create a strong password</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button size={"sm"} variant={"link"} asChild>
+              <Link href="/auth/reset">Forgot your password?</Link>
+            </Button>
+            <Button type="submit">Login</Button>
+          </form>
+        </Form>
+      </div>
     </AuthCard>
   );
 };
