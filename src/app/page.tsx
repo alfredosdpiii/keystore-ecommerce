f9a4909784b18b79cd73132c9beccb9189eb1d45
alfredosdpiii <alfredo.sdpiii@gmail.com>
2024-10-22 21:01:16 +0800

    clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,3 @@
-import getPosts from "@/server/actions/get-posts";
-
 export default async function Home() {
-  const { error, success } = await getPosts();
-
-  if (error) {
-    throw new Error(error);
-  }
-
-  if (success) {
-    return (
-      <main>
-        {success.map((post) => (
-          <div key={post.id}>
-            <h2>{post.title}</h2>
-          </div>
-        ))}
-      </main>
-    );
-  }
+  return <div>test</div>;
 }
